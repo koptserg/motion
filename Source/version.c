18f70ef1fa661098a50ad7c743dc3cca5e24b999
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2024-10-22 22:48:53 +0500

    Add files via upload

--- Source/version.c
+++ Source/version.c
@@ -7,8 +7,8 @@
     #endif
 
     #include "version.h"
-const uint8 zclApp_DateCode[] = { 16, '2', '2', '/', '0', '9', '/', '2', '0', '2', '2', ' ', '2', '0', ':', '4', '8' };
-const char zclApp_DateCodeNT[] = "22/09/2022 20:48";
+const uint8 zclApp_DateCode[] = { 16, '2', '2', '/', '1', '0', '/', '2', '0', '2', '4', ' ', '2', '2', ':', '0', '5' };
+const char zclApp_DateCodeNT[] = "22/10/2024 22:05";
 
     #ifdef __cplusplus
     }
